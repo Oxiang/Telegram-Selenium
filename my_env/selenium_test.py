f427d81bcb1499fc68d5ac32176fd70458fcd400
XiangQian <xiangqian_96@hotmail.com>
2019-06-17 22:51:33 +0800

    as

--- my_env/selenium_test.py
+++ my_env/selenium_test.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common.keys import Keys
 
 
 def test_sele():
+    chrome_options = Options()
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver = webdriver.Chrome()
     driver.get("http://www.python.org")
     assert "Python" in driver.title
